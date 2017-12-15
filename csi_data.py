bfc274c683c3d4cf366a69c4d1e291b2cf093f51
Todd Hendricks <hendricks.ta@gmail.com>
2017-12-15 03:34:49 -0800

    create data variable that captures entire dataset in list to create more managable data

--- csi_data.py
+++ csi_data.py
@@ -7,11 +7,9 @@ import xlrd
 
 book = xlrd.open_workbook('NSHP.xlsx')
 sheet = book.sheet_by_name('NSHP')
-count = 0
-data = {}
-for i in range(sheet.nrows):
-	if count < 10:
-		if i >= 14:
-			row = sheet.row_values(i)
-			zip = row[2]
-	print sheet.row_values(i) 
+data = [
+[sheet.cell_value(r,c)]
+for c in range(sheet.ncols)
+for r in range(sheet.nrows)
+]
+print type(data)
